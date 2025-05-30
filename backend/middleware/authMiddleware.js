71d9a6541e941193a928223daa252de43ba5c3b8
SeemantBosch <139146943+SeemantBosch@users.noreply.github.com>
2025-05-30 21:57:14 +0200

    rename to lower case

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/UserModel');
+const User = require('../models/userModel');
 
 const protect = async (req, res, next) => {
     let token;
